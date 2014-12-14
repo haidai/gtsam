b4b6a4e8dbb3f314dac22d47ec128e0b6ddcc88b
Andrew Melim <Andrew.Melim@gatech.edu>
2014-12-13 22:37:03 -0800

    Switching over to call python wrapper to build emitted wrap code for python module

--- cmake/GtsamPythonWrap.cmake
+++ cmake/GtsamPythonWrap.cmake
@@ -4,82 +4,105 @@ set(GTSAM_BUILD_PYTHON_FLAGS "" CACHE STRING "Extra flags for running Matlab PYT
 set(GTSAM_PYTHON_INSTALL_PATH "" CACHE PATH "Python toolbox destination, blank defaults to CMAKE_INSTALL_PREFIX/borg/python")
 if(NOT GTSAM_PYTHON_INSTALL_PATH)
   set(GTSAM_PYTHON_INSTALL_PATH "${CMAKE_INSTALL_PREFIX}/borg/python")
-endif() 
+endif()
 
 #Author: Paul Furgale Modified by Andrew Melim
-function(wrap_python TARGET_NAME PYTHON_MODULE_DIRECTORY)
+function(wrap_and_install_python TARGET_NAME PYTHON_MODULE_DIRECTORY)
+
+	# Paths for generated files
+	set(generated_files_path "${PROJECT_BINARY_DIR}/wrap/${moduleName}")
+	set(generated_cpp_file "${generated_files_path}/${moduleName}_wrapper.cpp")
+	set(compiled_mex_modules_root "${PROJECT_BINARY_DIR}/wrap/${moduleName}_mex")
+
+  # Set up generation of module source file
+	file(MAKE_DIRECTORY "${generated_files_path}")
+	add_custom_command(
+    TARGET wrap
+		DEPENDS ${interfaceHeader} wrap ${module_library_target} ${otherLibraryTargets} ${otherSourcesAndObjects}
+        COMMAND 
+            wrap
+            ${modulePath}
+            ${moduleName} 
+            ${generated_files_path} 
+            ${matlab_h_path} 
+            'python'
+		VERBATIM
+		WORKING_DIRECTORY ${generated_files_path})
+
+  #TODO(Andrew): Reimplement below
   # Boost
-  find_package(Boost COMPONENTS python filesystem system REQUIRED)
-  include_directories(${Boost_INCLUDE_DIRS})
+  #find_package(Boost COMPONENTS python REQUIRED)
+  #include_directories(${Boost_INCLUDE_DIRS})
 
-  # Find Python
-  find_package(PythonLibs 2.7 REQUIRED)
-  include_directories(${PYTHON_INCLUDE_DIRS})
+  ## Find Python
+  #find_package(PythonLibs 2.7 REQUIRED)
+  #include_directories(${PYTHON_INCLUDE_DIRS})
 
-  IF(APPLE)
-    # The apple framework headers don't include the numpy headers for some reason.
-    GET_FILENAME_COMPONENT(REAL_PYTHON_INCLUDE ${PYTHON_INCLUDE_DIRS} REALPATH)
-    IF( ${REAL_PYTHON_INCLUDE} MATCHES Python.framework)
-      message("Trying to find extra headers for numpy from ${REAL_PYTHON_INCLUDE}.")
-      message("Looking in ${REAL_PYTHON_INCLUDE}/../../Extras/lib/python/numpy/core/include/numpy")
-      FIND_PATH(NUMPY_INCLUDE_DIR arrayobject.h
-  ${REAL_PYTHON_INCLUDE}/../../Extras/lib/python/numpy/core/include/numpy
-  ${REAL_PYTHON_INCLUDE}/numpy
-  )
-      IF(${NUMPY_INCLUDE_DIR} MATCHES NOTFOUND)
-  message("Unable to find numpy include directories: ${NUMPY_INCLUDE_DIR}")
-      ELSE()
-  message("Found numpy headers at ${NUMPY_INCLUDE_DIR}")
-  INCLUDE_DIRECTORIES(${NUMPY_INCLUDE_DIR})
-  INCLUDE_DIRECTORIES(${NUMPY_INCLUDE_DIR}/..)
-      ENDIF()
-    ENDIF()
-  ENDIF(APPLE)
+  #IF(APPLE)
+  #  # The apple framework headers don't include the numpy headers for some reason.
+  #  GET_FILENAME_COMPONENT(REAL_PYTHON_INCLUDE ${PYTHON_INCLUDE_DIRS} REALPATH)
+  #  IF( ${REAL_PYTHON_INCLUDE} MATCHES Python.framework)
+  #    message("Trying to find extra headers for numpy from ${REAL_PYTHON_INCLUDE}.")
+  #    message("Looking in ${REAL_PYTHON_INCLUDE}/../../Extras/lib/python/numpy/core/include/numpy")
+  #    FIND_PATH(NUMPY_INCLUDE_DIR arrayobject.h
+  #${REAL_PYTHON_INCLUDE}/../../Extras/lib/python/numpy/core/include/numpy
+  #${REAL_PYTHON_INCLUDE}/numpy
+  #)
+  #    IF(${NUMPY_INCLUDE_DIR} MATCHES NOTFOUND)
+  #message("Unable to find numpy include directories: ${NUMPY_INCLUDE_DIR}")
+  #    ELSE()
+  #message("Found numpy headers at ${NUMPY_INCLUDE_DIR}")
+  #INCLUDE_DIRECTORIES(${NUMPY_INCLUDE_DIR})
+  #INCLUDE_DIRECTORIES(${NUMPY_INCLUDE_DIR}/..)
+  #    ENDIF()
+  #  ENDIF()
+  #ENDIF(APPLE)
 
-  if(MSVC)
-    add_library(${TARGET_NAME} MODULE ${ARGN})
-    set_target_properties(${TARGET_NAME} PROPERTIES 
-        OUTPUT_NAME         ${TARGET_NAME}
-        CLEAN_DIRECT_OUTPUT 1
-        VERSION             1
-        SOVERSION           0
-        SUFFIX              ".pyd")
-    target_link_libraries(${TARGET_NAME} ${Boost_PYTHON_LIBRARY} ${PYTHON_LIBRARY} gtsamDebug) #temp
+  #if(MSVC)
+  #  add_library(${TARGET_NAME} MODULE ${ARGN})
+  #  set_target_properties(${TARGET_NAME} PROPERTIES
+  #      OUTPUT_NAME         ${TARGET_NAME}
+  #      CLEAN_DIRECT_OUTPUT 1
+  #      VERSION             1
+  #      SOVERSION           0
+  #      SUFFIX              ".pyd")
+  #  target_link_libraries(${TARGET_NAME} ${Boost_PYTHON_LIBRARY} ${PYTHON_LIBRARY} gtsamDebug) #temp
 
-    set(PYLIB_OUTPUT_FILE $<TARGET_FILE:${TARGET_NAME}>)
-    message(${PYLIB_OUTPUT_FILE})
-    get_filename_component(PYLIB_OUTPUT_NAME ${PYLIB_OUTPUT_FILE} NAME_WE)
-    #set(PYLIB_SO_NAME ${PYLIB_OUTPUT_NAME}.pyd)
+  #  set(PYLIB_OUTPUT_FILE $<TARGET_FILE:${TARGET_NAME}>)
+  #  message(${PYLIB_OUTPUT_FILE})
+  #  get_filename_component(PYLIB_OUTPUT_NAME ${PYLIB_OUTPUT_FILE} NAME_WE)
+  #  #set(PYLIB_SO_NAME ${PYLIB_OUTPUT_NAME}.pyd)
 
-  ELSE()
-    # Create a shared library
-    add_library(${TARGET_NAME} SHARED ${ARGN})
+  #ELSE()
+  #  # Create a shared library
+  #  add_library(${TARGET_NAME} SHARED ${ARGN})
 
-    set_target_properties(${TARGET_NAME} PROPERTIES 
-        OUTPUT_NAME         ${TARGET_NAME}
-        CLEAN_DIRECT_OUTPUT 1
-        VERSION             1
-        SOVERSION           0)
-    target_link_libraries(${TARGET_NAME} ${Boost_PYTHON_LIBRARY} ${PYTHON_LIBRARY} gtsamDebug) #temp
-    # On OSX and Linux, the python library must end in the extension .so. Build this
-    # filename here.
-    #get_property(PYLIB_OUTPUT_FILE TARGET ${TARGET_NAME} PROPERTY LOCATION)
-    set(PYLIB_OUTPUT_FILE $<TARGET_FILE:${TARGET_NAME}>)
-    message(${PYLIB_OUTPUT_FILE})
-    get_filename_component(PYLIB_OUTPUT_NAME ${PYLIB_OUTPUT_FILE} NAME_WE)
-    set(PYLIB_SO_NAME ${PYLIB_OUTPUT_NAME}.so)
+  #  set_target_properties(${TARGET_NAME} PROPERTIES
+  #      OUTPUT_NAME         ${TARGET_NAME}
+  #      CLEAN_DIRECT_OUTPUT 1
+  #      VERSION             1
+  #      SOVERSION           0)
+  #  target_link_libraries(${TARGET_NAME} ${Boost_PYTHON_LIBRARY} ${PYTHON_LIBRARY} gtsamDebug) #temp
+  #  # On OSX and Linux, the python library must end in the extension .so. Build this
+  #  # filename here.
+  #  #get_property(PYLIB_OUTPUT_FILE TARGET ${TARGET_NAME} PROPERTY LOCATION)
+  #  set(PYLIB_OUTPUT_FILE $<TARGET_FILE:${TARGET_NAME}>)
+  #  message(${PYLIB_OUTPUT_FILE})
+  #  get_filename_component(PYLIB_OUTPUT_NAME ${PYLIB_OUTPUT_FILE} NAME_WE)
+  #  set(PYLIB_SO_NAME ${PYLIB_OUTPUT_NAME}.so)
 
-  ENDIF(MSVC)
+  #ENDIF(MSVC)
 
 
-  # Cause the library to be output in the correct directory.
-  add_custom_command(TARGET ${TARGET_NAME}
-    POST_BUILD
-    COMMAND cp -v ${PYLIB_OUTPUT_FILE} ${PYTHON_MODULE_DIRECTORY}/${PYLIB_SO_NAME}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMENT "Copying library files to python directory" )
+  ## Cause the library to be output in the correct directory.
+  #add_custom_command(TARGET ${TARGET_NAME}
+  #  POST_BUILD
+  #  COMMAND cp -v ${PYLIB_OUTPUT_FILE} ${PYTHON_MODULE_DIRECTORY}/${PYLIB_SO_NAME}
+  #  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+  #  COMMENT "Copying library files to python directory" )
 
-  get_directory_property(AMCF ADDITIONAL_MAKE_CLEAN_FILES)
-  list(APPEND AMCF ${PYTHON_MODULE_DIRECTORY}/${PYLIB_SO_NAME})
-  set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${AMCF}") 
+  #get_directory_property(AMCF ADDITIONAL_MAKE_CLEAN_FILES)
+  #list(APPEND AMCF ${PYTHON_MODULE_DIRECTORY}/${PYLIB_SO_NAME})
+  #set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES "${AMCF}")
+
 endfunction(wrap_python)
