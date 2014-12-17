76ba1ff0f382b88dfb97d0f1946c1905f8d7f000
Andrew Melim <Andrew.Melim@gatech.edu>
2014-12-17 11:06:08 -0800

    Overloaded constructor

--- python/gtsam_tests/testPoint2.py
+++ python/gtsam_tests/testPoint2.py
@@ -3,6 +3,7 @@ from gtsam import *
 
 #https://docs.python.org/2/library/unittest.html
 class TestPoint2(unittest.TestCase):
+
     def setUp(self):
         self.point = Point2()
 
@@ -11,6 +12,14 @@ class TestPoint2(unittest.TestCase):
         y = self.point.y()
         self.assertEqual(x, 0.0)
         self.assertEqual(y, 0.0)
+
+    def test_overloaded_constructor(self):
+        p = Point2(1.0, 1.0)
+        x = p.x()
+        y = p.y()
+        self.assertEqual(x, 1.0)
+        self.assertEqual(y, 1.0)
+
 
 if __name__ == '__main__':
     unittest.main()
