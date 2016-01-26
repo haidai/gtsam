22377de03331c39dc2b7bda7b9a25fb40f37a86f
dellaert <dellaert@cc.gatech.edu>
2016-01-26 10:12:33 -0800

    Added a few tests

--- python/gtsam_tests/testPoint2.py
+++ python/gtsam_tests/testPoint2.py
@@ -2,13 +2,18 @@ import unittest
 import gtsampy as gtsam
 import numpy as np
 
-#https://docs.python.org/2/library/unittest.html
 class TestPoint2(unittest.TestCase):
     def setUp(self):
         self.point = gtsam.Point2()
 
-    def test_constructor(self):
-        pass
+    def test_getters(self):
+        self.assertEqual(0, self.point.x())
+        self.assertEqual(0, self.point.y())
+
+    def test_constructors(self):
+        p = gtsam.Point2(1, 2)
+        self.assertEqual(1, p.x())
+        self.assertEqual(2, p.y())
 
 if __name__ == '__main__':
     unittest.main()
